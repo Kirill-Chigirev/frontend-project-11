0103a84f73836b94e901067b8a288704b1bd13d5
Kirill Chigirev <zhebelme@MacBook-Air-Kirill.local>
2025-02-26 19:51:42 +0700

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { GenerateSW } from 'workbox-webpack-plugin';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(process.cwd(), 'dist'),
+    // clean: true,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
       },
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
@@ -27,14 +46,19 @@ export default {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
         use: 'file-loader',
       },
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html',
-    }),
-  ],
-  output: {
-    clean: true,
-  },
+};
+
+export default () => {
+  if (isProduction) {
+    config.mode = 'production';
+
+    config.plugins.push(new GenerateSW());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
