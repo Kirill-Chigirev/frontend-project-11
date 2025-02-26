016969a9bb14d6ed13b8c175d93428863aa4e2f4
Kirill Chigirev <zhebelme@MacBook-Air-Kirill.local>
2025-02-26 22:58:01 +0700

    added ignoring warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-import { resolve } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { GenerateSW } from 'workbox-webpack-plugin';
 
@@ -9,9 +6,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: resolve(process.cwd(), 'dist'),
-    // clean: true,
+    clean: true,
   },
   devServer: {
     open: true,
@@ -19,37 +14,23 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template.html',
+      template: 'index.html',
     }),
   ],
   module: {
     rules: [
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-
-      {
-        test: /\.html$/i,
-        use: ['html-loader'],
-      },
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
+  ignoreWarnings: [
+    {
+      message: /Module Warning \(from .+sass-loader.+\)/,
+    },
+  ],
 };
 
 export default () => {
